66fd20ea44b19496e3827655c7531150d52c71c1
JTLiner925 <[jtliner925@gmail.com]>
2020-04-17 13:45:54 -0500

    working on test files using enzyme

--- src/shopping-list/ShoppingItem.test.js
+++ src/shopping-list/ShoppingItem.test.js
@@ -1,1 +1,37 @@
+//npm install enzyme enzyme-adapter-react-16 --save-dev
+//this installs enzyme before you decide to use it
 
+//npm install enzyme-to-json --save-dev
+//need to install this command as well
+
+// import { configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// configure({ adapter: new Adapter() });
+//put this code in the setupTest.js file
+
+import React from 'react';
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json';
+import ShoppingItem from './ShoppingItem';
+
+//describe creates test-suite
+describe('ShoppingItem component', () => {
+  it('renders empty given no item', () => {
+    const wrapper = shallow(<ShoppingItem />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  }) 
+
+  it('renders the item when supplied', () => {
+    // We're adding props to the ShoppingItem for different test cases
+    const testItem = { name: 'test-item', checked: false }
+    const wrapper = shallow(<ShoppingItem item={testItem} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  }) 
+
+  it('strikes through checked items', () => {
+    const testItem = { name: 'checked-item', checked: true }
+    const wrapper = shallow(<ShoppingItem item={testItem} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
